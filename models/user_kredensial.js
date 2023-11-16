561a5da62331957988f282e88e39e4c6f57ef028
24aeronyx <11211017@student.itk.ac.id>
2023-11-16 19:25:09 +0800

    Setup folder dan membuat migration untuk user_kredensial

---
+++ models/user_kredensial.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User_kredensial extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User_kredensial.init({
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: {
+      type: DataTypes.ENUM('pasien', 'dokter', 'admin'),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User_kredensial',
+  });
+  return User_kredensial;
+};
