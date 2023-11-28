d26199439e320813548903d22f360f95093dfbe7
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-29 04:00:14 +0700

    membuat auth controllers belum selesai

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,50 +5,41 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     Login: async (req, res) => {
-      try{
-        const userKredensial = await User_kredensial.findAll({
-          where:{
-            email: req.body.email,
-          }
-        });
-        if(!userKredensial) return res.status(404).json({message: "User tidak ditemukan"});
-        const match = await bcrypt.compareSync(userKredensial.password, req.body.password);
-        if(!match) return res.status(400).json({message: "Password salah"});
-        const userKredensialId = userKredensial[0].id;
-        const username = userKredensial[0].username;
-        const email = userKredensial[0].email;
-        const role = userKredensial[0].role
-        const accessToken =jwt.sign({userKredensialId,username,email,password, role},process.env.JWT_KEY ) 
-           res.json({
-            message: "berhasil login",
-            userKredensialId,
-            accessToken,
-            role
-           })
-      }catch (error) {
-        res.json(error.message)
-      }
+      const user = await User_kredensial.findOne({
+        where: {
+          email: req.body.email
+        }
+      });
+      if(!user) return res.status(404).json({message: "User tidak ditemukan"});
+    const match = await bcrypt.compareSync(user.password, req.body.password);
+    if(!match) return res.status(400).json({message: "Password Salah"});
+    req.session.userId = user.id;
+    const id = user.id;
+    const username = user.username;
+    const email = user.email;
+    const role = user.role;
+    res.status(200).json({id, username, email, role});
     },
 
     Me: async (req, res) => {
-      if(!req.session.userKredensialId){
-        return res.status(401).json({message: "Silahkan login ke Akun Anda"});
-    }
-    const userKredensial = await User_kredensial.findOne({
+      if(!req.session.userId) {
+        return res.status(401).json({message: "Silahkan Login ke akun Anda!"});
+      }
+      const user = await User_kredensial.findOne({
         attributes: ['id', 'username', 'email', 'role'],
         where: {
-          id : req.session.userKredensialId
+          id: req.session.userId
         }
       });
-      if(!userKredensial) return res.status(404).json({message: "User Tidak ditemukan"});
-      res.status(200).json(userKredensial)
+      if(!user) return res.status(404).json({message: "User tidak ditemukan"});
+      res.status(200).json(user);
     },
 
     Logout: (req, res) => {
       req.session.destroy((err) => {
         if(err) return res.status(400).json({message: "Tidak dapat logout"});
-        res.status(200).json({message: "Anda telah logout"});
-    })
-    },
+        res.status(200).json({message: "Anda telah logout"})
+      })
+    }
       
 }
