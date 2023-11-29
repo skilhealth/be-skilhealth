653d195fdf7612d1867f7c137bd5515bb9e95cd0
24aeronyx <11211017@student.itk.ac.id>
2023-11-29 16:52:12 +0800

    Remove dummy content

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const route = express.Router()
 const doktorRoutes = require("./doctor-routes")
 const bookingRoutes = require("./booking-routes")
 const ujilabRoutes = require("./ujilab-routes")
+const forumRoute = require('./forum-route')
 const { Instansi, Spesialis, Jadwal } = require("../models")
 const { DATE } = require("sequelize")
 
@@ -11,45 +12,11 @@ route.get("/", (req, res) => {
         message: "Selamat datang di express"
     })
 })
+
+
+route.use("/forum", forumRoute)
 route.use("/doctors", doktorRoutes)
 route.use("/bookings", bookingRoutes)
 route.use("/ujilab", ujilabRoutes)
-route.post("/rs", async (req, res) => {
-    const data = await Instansi.create({
-        nama: "Skilvul Hospital Jakarta",
-        alamat: "Jakarta",
-        no_tlp: "0123",
-        area: "Jabodetabek",
-        email: "@gmail.com"
-    })
-    res.json({
-        message: "Berhasil",
-        data: data
-    })
-})
-route.post("/sp", async (req, res) => {
-    const data = await Spesialis.create({
-        nama: "Ahli Jantung",
-        keterangan: "-",
-    })
-    res.json({
-        message: "Berhasil",
-        data: data
-    })
-})
-route.post("/jd", async (req, res) => {
-    const data = await Jadwal.create({
-        dokter_id: 2,
-        date:"2023-11-24",
-        tipe: "reguler",
-        keterangan:"-",
-        status: true,
-        harga:46000
-    })
-    res.json({
-        message: "Berhasil",
-        data: data
-    })
-})
 
 module.exports = route
