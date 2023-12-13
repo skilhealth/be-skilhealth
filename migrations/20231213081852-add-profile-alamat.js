cfc60915069d8c95045b35dd47d43d9e40c8ad5f
awanvirgi <virgiawan.dev15@gmail.com>
2023-12-13 17:06:36 +0700

    add migrate and edit user controler

---
+++ migrations/20231213081852-add-profile-alamat.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn(
+        'User',
+        'alamat',
+        {
+          type: Sequelize.TEXT,
+        },
+        { transaction }
+      );
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn('User', 'alamat', { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
